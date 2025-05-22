d46f6f46a8f36b6099deee1339167c887d9c6aec
josefary <josefary@student.21-school.ru>
2025-05-22 15:40:22 +0300

    add test_solution.py for task1 solution

--- task1/solution.py
+++ task1/solution.py
@@ -1,4 +1,4 @@
-from constans import TYPE_ERROR_TEXT
+TYPE_ERROR_TEXT = "argument '{argument}' must be '{correct_type}', not '{wrong_type}'"
 
 def strict(func):
     def wrapper(*args, **kwargs):
@@ -6,26 +6,26 @@ def strict(func):
 
         for i, (arg_name, type_name) in enumerate(annotations.items()):
             if i < len(args):
-                if type_name and not isinstance(args[i], type_name):
+                if type_name and not type(args[i]) is type_name:  # строгая проверка т.к. не требуется поддержка наследования
                     raise TypeError(
                         TYPE_ERROR_TEXT.format(
                             argument=arg_name,
                             correct_type=type_name.__name__,
                             wrong_type=type(args[i]).__name__
-                            )
+                        )
                     )
-                
+
         for arg_name, value in kwargs.items():
             type_name = annotations.get(arg_name)
-            if type_name and not isinstance(value, type_name):
+            if type_name and not type(value) is type_name:  # строгая проверка т.к. не требуется поддержка наследования
                 raise TypeError(
                     TYPE_ERROR_TEXT.format(
                         argument=arg_name,
                         correct_type=type_name.__name__,
                         wrong_type=type(value).__name__
-                        )
+                    )
                 )
-        
+
         return func(*args, **kwargs)
     return wrapper
 
@@ -35,8 +35,6 @@ def sum_two(a: int, b: int) -> int:
 
 def main() -> int:
     print(sum_two(1, 2))
-    print(sum_two(a=1, b=2))
-    print(sum_two(1.1, "str"))
 
 if __name__ == '__main__':
     main()
---
+++ task1/test_solution.py
@@ -0,0 +1,56 @@
+import pytest
+from solution import strict, TYPE_ERROR_TEXT
+
+def test_strict_correct_types():
+    @strict
+    def sum_two(a: int, b: int) -> int:
+        return a + b
+
+    assert sum_two(1, 2) == 3
+    assert sum_two(a=1, b=2) == 3
+
+def test_strict_incorrect_args():
+    @strict
+    def concat_two(s1: str, s2: str) -> str:
+        return f'{s1} {s2}' 
+
+    with pytest.raises(TypeError) as e_message:
+        concat_two('Hello', 1)
+
+    assert e_message.value.args[0] == TYPE_ERROR_TEXT.format(
+        argument='s2', correct_type='str', wrong_type='int'
+    )
+
+def test_strict_incorrect_kwargs():
+    @strict
+    def sum_two(a: float, b: float) -> float:
+        return a + b
+
+    with pytest.raises(TypeError) as exc_info:
+        sum_two(a=1.0, b="2.0")
+
+    assert exc_info.value.args[0] == TYPE_ERROR_TEXT.format(
+        argument='b', correct_type='float', wrong_type='str'
+    )
+
+def test_strict_no_annotations():
+    @strict
+    def no_annotations(a, b):
+        return a + b
+
+    assert no_annotations(1, 2) == 3
+    assert no_annotations(a='a', b='b') == 'ab'
+
+def test_strict_partial_annotations():
+    @strict
+    def partial_annotations(a: int, b):
+        return a + b
+
+    assert partial_annotations(1, 2) == 3
+    assert partial_annotations(1, 2.0) == 3.0
+
+    with pytest.raises(TypeError):
+        partial_annotations(1.0, 2)
+
+if __name__ == '__main__':
+    pytest.main()
